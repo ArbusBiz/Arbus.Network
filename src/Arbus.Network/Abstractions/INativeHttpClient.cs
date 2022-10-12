cdee7e24d37dab616d034bd0bd02e79a3a437141
Sokolov Max <maxerdan@yandex.ru>
2022-10-12 13:43:18 +0500

    Hide Send method

--- src/Arbus.Network/Abstractions/INativeHttpClient.cs
+++ src/Arbus.Network/Abstractions/INativeHttpClient.cs
@@ -2,7 +2,6 @@
 {
     public interface INativeHttpClient
     {
-        Task<HttpResponseMessage> Send(HttpRequestMessage httpRequest, CancellationToken timeout, HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseHeadersRead);
         Task<HttpResponseMessage> SendRequest(HttpRequestMessage request, CancellationToken cancellationToken);
         Task<string> GetString(string url, TimeSpan? timeout = default);
         Task<string> GetString(Uri uri, TimeSpan? timeout = default);
