0f62cce341dfc1ff50d9f09c380b1e7ff65e43b3
Artem Levin <64748654+Leo506@users.noreply.github.com>
2023-10-17 12:18:19 +0500

    Remove NativeHttpClient.GetString() (#55)
    Co-authored-by: Artem Levin <alevin@arbus.biz>

--- src/Arbus.Network/INativeHttpClient.cs
+++ src/Arbus.Network/INativeHttpClient.cs
@@ -3,6 +3,4 @@
 public interface INativeHttpClient
 {
     Task<HttpResponseMessage> SendRequest(HttpRequestMessage request, CancellationToken cancellationToken, HttpCompletionOption httpCompletionOption = HttpCompletionOption.ResponseHeadersRead);
-    Task<string> GetString(string uri, TimeSpan? timeout = default);
-    Task<string> GetString(Uri uri, TimeSpan? timeout = default);
 }
